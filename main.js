bf55b38399c449d9892e61091cc0c79b85907355
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-25 22:13:08 -0400

    practicing jQuery

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	$("body").hide()
+
+	$("body").click(function() {
+		$(".header").show();
+
+	});
+
+
+	// $(".header").mouseenter(function() {
+	// 	$(".header").slideUp(2000);
+	// 	$(".header").slideDown(2000);
+	// 	$("#crazy").fadeOut(1000);
+	// 	$("#crazy").fadeIn(1000);	
+	// 	$("#crazy").fadeOut(1000);
+	// 	$("#crazy").fadeIn(1000);
+	// 	$("#crazy").fadeOut(1000);
+	// 	$("#crazy").fadeIn(1000);
+	// });
+
+	// $(".content-1").hover(function() {
+	// 	$(".p-one").fadeOut(2000);
+	// });
+
+
+
+
+	// $(".container").hide()
+
+});

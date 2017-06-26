e6519d39286c252017c0051d7881d7dcda38652e
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-25 23:54:46 -0400

    commented out my tests and settled on some jQuery code that works for the header even tho it's not what i want

--- main.js
+++ main.js
@@ -1,74 +1,57 @@
 $(document).ready(function() {
 
-	$("body").mouseenter(function() {
-		$(".container").fadeOut();	
-	});
+	// $(".container").hide()
 
-	
-	// $("body").mouseenter(function() {
-	// 	$(".container").fadeOut(function() {
-	// 		$("body").click(function() {
-	// 			$(".container").fadeIn();
-	// 		});
+// Trying to get mouseenter or click to prompt 
+// content dispalying, attempted to set default
+// for container to display: none but still didn't work
+	// 	$("body").mouseenter(function() {
+	// 		$(".container").show();	
 	// 	});
-	// });
-
-});
 
 
-// $("body").click(function() {
-// 	$(".container").show()
-// });
-
-		// function() {
-		// $("body").click(function() {
-		// 	$("body").show()
-		// });
-
-
-	// $("body").click(function() {
-	// 	$(".header").hide(function() {
-	// 		$("body").click(function() {
-	// 			$(".header").show(function() {
-	// 				$("body").click(function() {
-	// 					$(".header").slideUp(2000);
-	// 					$(".header").slideDown(2000);
-	// 					$("#crazy").fadeOut(1000);
-	// 					$("#crazy").fadeIn(1000);	
-	// 					$("#crazy").fadeOut(1000);
-	// 					$("#crazy").fadeIn(1000);
-	// 					$("#crazy").fadeOut(1000);
-	// 					$("#crazy").fadeIn(1000);
-	// 				});
+// Fade out on mouseenter works but can't get
+// fade in to work at all
+	// 	$("body").mouseenter(function() {
+	// 		$(".container").fadeOut(function() {
+	// 			$("body").click(function() {
+	// 				$(".container").fadeIn();
 	// 			});
 	// 		});
 	// 	});
 
 
-	// $(".header").mouseenter(function() {
-	// $(".header").slideUp(2000);
-	// $(".header").slideDown(2000);
-	// $("#crazy").fadeOut(1000);
-	// $("#crazy").fadeIn(1000);	
-	// $("#crazy").fadeOut(1000);
-	// $("#crazy").fadeIn(1000);
-	// $("#crazy").fadeOut(1000);
-	// $("#crazy").fadeIn(1000);
-	// });
+
+
+// This works but after click-hide, click show
+// it seems to hide&show again before the slides
+// and fades start?
+	$("body").click(function() {
+		$(".header").hide(function() {
+			$("body").click(function() {
+				$(".header").show(function() {
+					$("body").click(function() {
+						$(".header").slideUp(2000);
+						$(".header").slideDown(2000);
+						$("#crazy").fadeOut(1000);
+						$("#crazy").fadeIn(1000);	
+						$("#crazy").fadeOut(1000);
+						$("#crazy").fadeIn(1000);
+						$("#crazy").fadeOut(1000);
+						$("#crazy").fadeIn(1000);
+					});
+				});
+			});
+		});
+	});
+
+
+
+	$(".content-1").hover(function() {
+		$(".p-one").fadeOut(2000);
+		$("")
+	});
+
 
 	
-
-	// });
-
-
-
-
-	// $(".content-1").hover(function() {
-	// 	$(".p-one").fadeOut(2000);
-	// });
-
-
-
-
-	// $(".container").hide()
-
+});

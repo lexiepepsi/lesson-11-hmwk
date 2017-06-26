fb2d10da81b82daf0ad08a2fad24511743fb42d3
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-25 23:42:29 -0400

    trying to get html content to be hidden then display on clikc or mouse-enter

--- main.js
+++ main.js
@@ -1,23 +1,67 @@
 $(document).ready(function() {
 
-	$("body").hide()
-
-	$("body").click(function() {
-		$(".header").show();
-
+	$("body").mouseenter(function() {
+		$(".container").fadeOut();	
 	});
+
+	
+	// $("body").mouseenter(function() {
+	// 	$(".container").fadeOut(function() {
+	// 		$("body").click(function() {
+	// 			$(".container").fadeIn();
+	// 		});
+	// 	});
+	// });
+
+});
+
+
+// $("body").click(function() {
+// 	$(".container").show()
+// });
+
+		// function() {
+		// $("body").click(function() {
+		// 	$("body").show()
+		// });
+
+
+	// $("body").click(function() {
+	// 	$(".header").hide(function() {
+	// 		$("body").click(function() {
+	// 			$(".header").show(function() {
+	// 				$("body").click(function() {
+	// 					$(".header").slideUp(2000);
+	// 					$(".header").slideDown(2000);
+	// 					$("#crazy").fadeOut(1000);
+	// 					$("#crazy").fadeIn(1000);	
+	// 					$("#crazy").fadeOut(1000);
+	// 					$("#crazy").fadeIn(1000);
+	// 					$("#crazy").fadeOut(1000);
+	// 					$("#crazy").fadeIn(1000);
+	// 				});
+	// 			});
+	// 		});
+	// 	});
 
 
 	// $(".header").mouseenter(function() {
-	// 	$(".header").slideUp(2000);
-	// 	$(".header").slideDown(2000);
-	// 	$("#crazy").fadeOut(1000);
-	// 	$("#crazy").fadeIn(1000);	
-	// 	$("#crazy").fadeOut(1000);
-	// 	$("#crazy").fadeIn(1000);
-	// 	$("#crazy").fadeOut(1000);
-	// 	$("#crazy").fadeIn(1000);
+	// $(".header").slideUp(2000);
+	// $(".header").slideDown(2000);
+	// $("#crazy").fadeOut(1000);
+	// $("#crazy").fadeIn(1000);	
+	// $("#crazy").fadeOut(1000);
+	// $("#crazy").fadeIn(1000);
+	// $("#crazy").fadeOut(1000);
+	// $("#crazy").fadeIn(1000);
 	// });
+
+	
+
+	// });
+
+
+
 
 	// $(".content-1").hover(function() {
 	// 	$(".p-one").fadeOut(2000);
@@ -28,4 +72,3 @@ $(document).ready(function() {
 
 	// $(".container").hide()
 
-});
